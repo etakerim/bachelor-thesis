222306ba884d64be648820272ca2e016d6a4b37d
Miroslav Hájek <xhajekm@stuba.sk>
2021-10-23 17:12:19 +0200

    Blink LED on icomox

--- icomox/download-libs.sh
+++ icomox/download-libs.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 # Skript na stiahnutie knižníc a zmazanie nepotrebných súčastí
-
 # AnalogDevices.ADuCM4x50_DFP.4.0.0   https://developer.arm.com/tools-and-software/embedded/cmsis/cmsis-packs
 # ARM.CMSIS.5.8.0                     https://github.com/ARM-software/CMSIS_5/releases/latest
-# FATFS                               http://elm-chan.org/fsw/ff/00index_e.html
-# FreeRTOS                            https://www.freertos.org/a00104.html
 
 HAL_DIR="ADuCM4x50"
 HAL_SOURCE="AnalogDevices.ADuCM4x50_DFP.4.0.0.pack"
 CMSIS_DIR="CMSIS"
 CMSIS_SOURCE="ARM.CMSIS.5.8.0.pack"
-RTOS_DIR="FreeRTOS"
-RTOS_SOURCE="FreeRTOSv202012.02-LTS.zip"
-FATFS_DIR="FatFS"
-FATFS_SOURCE="ff14b.zip"
+OPENOCD_CODE="openocd"
+OPENOCD_PATCH="4f9cfbd.fixed.patch"
 
 wget "https://download.analog.com/tools/EZBoards/ADuCM4050/Releases/$HAL_SOURCE"
 wget "https://github.com/ARM-software/CMSIS_5/releases/download/5.8.0/$CMSIS_SOURCE"
-wget "http://elm-chan.org/fsw/ff/arc/$FATFS_SOURCE"
-wget "https://github.com/FreeRTOS/FreeRTOS-LTS/releases/download/202012.02-LTS/$RTOS_SOURCE"
+git clone "https://github.com/ArrowElectronics/iCOMOX.git"
+git clone "https://git.code.sf.net/p/openocd/code" "$OPENOCD_CODE"
 
 unzip -d "${HAL_DIR}" "$HAL_SOURCE"
 rm -r "${HAL_DIR}/ARM" "${HAL_DIR}/Boards" "${HAL_DIR}/Flash" "${HAL_DIR}/tools" \
@@ -30,18 +25,38 @@ cp "${CMSIS_DIR}/LICENCE.txt" "${CMSIS_DIR}/CMSIS/LICENCE.txt"
 cp -r "${CMSIS_DIR}/CMSIS/" ./
 rm -r "${CMSIS_DIR}/CMSIS/" "${CMSIS_DIR}/Core_A" "${CMSIS_DIR}/DAP" "${CMSIS_DIR}/NN" "${CMSIS_DIR}/RTOS" "${CMSIS_DIR}/RTOS2" \
       "${CMSIS_DIR}/Driver" "${CMSIS_DIR}/Utilities" "${CMSIS_DIR}/Device" "${CMSIS_DIR}/ARM.CMSIS.pdsc" "${CMSIS_DIR}/ARM.CMSIS.sha1"
+      
+cp "$OPENOCD_PATCH" "$OPENOCD_CODE/"
+cd "$OPENOCD_CODE"
+git apply --stat "$OPENOCD_PATCH"
+git apply --check "$OPENOCD_PATCH"
+git am < "$OPENOCD_PATCH"
+./bootstrap
+./configure
+make
+sudo make install
+cd ..
 
-unzip -d "${RTOS_DIR}" "$RTOS_SOURCE"
-cp -r "${RTOS_DIR}/FreeRTOS-LTS/FreeRTOS/FreeRTOS-Kernel/" ./
-rm -r "$RTOS_DIR"
-cp -r "FreeRTOS-Kernel" "$RTOS_DIR"
-rm -r "FreeRTOS-Kernel"
-mkdir "${RTOS_DIR}/portable-temp"
-cp -r "${RTOS_DIR}/portable/GCC/ARM_CM4F/" "${RTOS_DIR}/portable/MemMang/" "${RTOS_DIR}/portable-temp"
-rm -r "${RTOS_DIR}/portable"
-cp -r "${RTOS_DIR}/portable-temp/" "${RTOS_DIR}/portable"
-rm -r "${RTOS_DIR}/portable-temp"
+   
+   
+#RTOS_DIR="FreeRTOS"
+#RTOS_SOURCE="FreeRTOSv202012.02-LTS.zip"
+#FATFS_DIR="FatFS"
+#FATFS_SOURCE="ff14b.zip"
 
-unzip -d "${FATFS_DIR}" "$FATFS_SOURCE"
+#wget "http://elm-chan.org/fsw/ff/arc/$FATFS_SOURCE"
+#wget "https://github.com/FreeRTOS/FreeRTOS-LTS/releases/download/202012.02-LTS/$RTOS_SOURCE"
+
+#unzip -d "${RTOS_DIR}" "$RTOS_SOURCE"
+#cp -r "${RTOS_DIR}/FreeRTOS-LTS/FreeRTOS/FreeRTOS-Kernel/" ./
+#rm -r "$RTOS_DIR"
+#cp -r "FreeRTOS-Kernel" "$RTOS_DIR"
+#rm -r "FreeRTOS-Kernel"
+#mkdir "${RTOS_DIR}/portable-temp"
+#cp -r "${RTOS_DIR}/portable/GCC/ARM_CM4F/" "${RTOS_DIR}/portable/MemMang/" "${RTOS_DIR}/portable-temp"
+#rm -r "${RTOS_DIR}/portable"
+#cp -r "${RTOS_DIR}/portable-temp/" "${RTOS_DIR}/portable"
+#rm -r "${RTOS_DIR}/portable-temp"
+#unzip -d "${FATFS_DIR}" "$FATFS_SOURCE"
 
 
--- icomox/src/main.c
+++ icomox/src/main.c
@@ -1,61 +1,46 @@
 #include <adi_processor.h>
 #include <pwr/adi_pwr.h>
 #include <gpio/adi_gpio.h>
-#include <spi/adi_spi.h>
-#include <uart/adi_uart.h>
-#include <arm_math.h>
-#include <FreeRTOS.h>
 
-#define IDLE_TASK_SIZE 1000
-
-// https://mcuoneclipse.com/2016/05/29/freertos-v9-0-0-with-static-memory-allocation/
-
-#if configSUPPORT_STATIC_ALLOCATION
-/* static memory allocation for the IDLE task */
-static StaticTask_t xIdleTaskTCBBuffer;
-static StackType_t xIdleStack[IDLE_TASK_SIZE];
- 
-void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize) {
-  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
-  *ppxIdleTaskStackBuffer = &xIdleStack[0];
-  *pulIdleTaskStackSize = IDLE_TASK_SIZE;
-}
-#endif
-
-#if configSUPPORT_STATIC_ALLOCATION && configUSE_TIMERS
-static StaticTask_t xTimerTaskTCBBuffer;
-static StackType_t xTimerStack[configTIMER_TASK_STACK_DEPTH];
- 
-/* If static allocation is supported then the application must provide the
-   following callback function - which enables the application to optionally
-   provide the memory that will be used by the timer task as the task's stack
-   and TCB. */
-void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize) {
-  *ppxTimerTaskTCBBuffer = &xTimerTaskTCBBuffer;
-  *ppxTimerTaskStackBuffer = &xTimerStack[0];
-  *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
-}
-#endif
-
+uint32_t gpio_mem[ADI_GPIO_MEMORY_SIZE];
 
 int main(void)
 {
-    // MCU: ADuCM4050 (ARM Cortex-M4F) w/Flash: 512 kB, SRAM: 128 kB, 12-bit SAR ADC (1.8 MSPS, 8 ch)
-    // iCOMOX SMIP
-    // Vibration sensor: ADXL356 (A2D interface)
-    // Low-power vibration sensor: ADXL362 (SPI interface)
-    // SmartMesh IP (2.4 GHz, 802.15.4e): LTC5800-IPM  (UART interface)
-    // microSD (SPI inteface)
+    adi_pwr_Init();
+    adi_pwr_SetClockDivider(ADI_CLOCK_HCLK, 1);
+    adi_pwr_SetClockDivider(ADI_CLOCK_PCLK, 1);
     
-    // Components:
-    // Network receive: Rules and Queries
-    // Network transmit: Result sets
-    // Read bus: Accelerometers
-    // Processing: Act on queries - Extract frequencies, Compress data
-    // Logging: micro SD card
+    adi_gpio_Init(gpio_mem, ADI_GPIO_MEMORY_SIZE);
+    adi_gpio_OutputEnable(ADI_GPIO_PORT2, ADI_GPIO_PIN_8, 1);
     
-    while (1)
-        ;
+    // GPIO42 (P2_10), GPIO40 (P2_08) - LED st. 39 MCU manuálu
+    while (1) {
+        adi_gpio_SetHigh(ADI_GPIO_PORT2, ADI_GPIO_PIN_8);
+        for (volatile int i = 0; i < 10000000; i++)
+            ;
+        adi_gpio_SetLow(ADI_GPIO_PORT2, ADI_GPIO_PIN_8);
+        for (volatile int i = 0; i < 10000000; i++)
+            ;
+    }
+    
+    adi_gpio_UnInit();
 }
+
+
+/*
+ * MCU: ADuCM4050 (ARM Cortex-M4F) w/Flash: 512 kB, SRAM: 128 kB, 12-bit SAR ADC (1.8 MSPS, 8 ch)
+ * iCOMOX SMIP
+ * Vibration sensor: ADXL356 (A2D interface)
+ * Low-power vibration sensor: ADXL362 (SPI interface)
+ * SmartMesh IP (2.4 GHz, 802.15.4e): LTC5800-IPM  (UART interface)
+ * microSD (SPI inteface)
+ * 
+ * Components:
+ * Network receive: Rules and Queries
+ * Network transmit: Result sets
+ * Read bus: Accelerometers
+ * Processing: Act on queries - Extract frequencies, Compress data
+ * Logging: micro SD card
+ */
 
 
