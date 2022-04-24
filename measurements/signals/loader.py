11799e2f58227f6c36588aee065121ef8f50f8bf
Miroslav Hájek <xhajekm@stuba.sk>
2022-04-24 19:51:22 +0200

    Transform datasets and start implementation in thesis

--- measurements/signals/loader.py
+++ measurements/signals/loader.py
@@ -1,37 +1,20 @@
 import pandas as pd
 
 
-def load_dataset(source, dataset, G=9.80665):
+def load_dataset(dataset):
     """
     Výstupom je tabuľka s troma atribútmi osí akcelerácie [m/s^2]: x, y, z a časovým indexom t [s]
     """
-    if source == 'r-das': 
-        FS_HZ = 952
-        PATH = 'datasets/'
+    PATH = 'datasets/'
+    FS_HZ = 500
 
-        measurement = pd.read_csv(PATH + dataset, sep='\t', skiprows=2)
-        measurement = measurement[['A_X [mg] ', ' A_Y [mg] ', ' A_Z [mg] ']].rename(columns={
-            'A_X [mg] ': 'x', ' A_Y [mg] ': 'y', ' A_Z [mg] ': 'z'
-        })
-        measurement['t'] = measurement.index * (1 / FS_HZ)
-        measurement.set_index('t', inplace=True)
-        return measurement.transform(lambda v: (v / 1000) * G), FS_HZ   # Premena z mg na m/s^2
+    measurement = pd.read_csv(PATH + dataset, sep=' ')
+    measurement['t'] = measurement.index * (1 / FS_HZ)
+    measurement.set_index('t', inplace=True)
 
-    elif source == 'esp':
-        FS_HZ = 500
-        MG_LSB = 0.061
-        PATH = 'datasets/'
+    return measurement, FS_HZ
 
-        measurement = pd.read_csv(PATH + dataset, sep=' ').rename(columns={
-            'A_X[LSB]': 'x', 'A_Y[LSB]': 'y', 'A_Z[LSB]': 'z'
-        })
-        measurement['t'] = measurement.index * (1 / FS_HZ)
-        measurement.set_index('t', inplace=True)
-        return measurement.transform(lambda v: ((v * MG_LSB) / 1000) * G), FS_HZ   # Premena z LSB na m/s^2
-    
-    else:
-        raise NotImplementedError('Neplatný názov datasetu')
-        
+
 # Odstrániť chybné hodnoty mimo rozsah: 
 # vibrations[vibrations['y'].abs() > 2*G]
 # measurement[measurement.isnull().any(axis=1)]
